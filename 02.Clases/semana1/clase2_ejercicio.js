60ba0d9d7e6ac3d705e3381f791da91ccdca4848
DabirGomezSerna <dasego.gomezserna@gmail.com>
2025-08-13 20:13:35 -0600

    Base excercise and new class excercise added

---
+++ 02.Clases/semana1/clase2_ejercicio.js
@@ -0,0 +1,21 @@
+const { ask } = require('../helpers/input.js');
+
+async function main() {
+ const age = Number(await ask("¿Cuántos años tienes?"));
+ const genero = await ask("¿Cuál es tu género (masculino/femenino)?");
+
+ if(age <= 12 && genero === "femenino"){
+    console.log("Eres una niña");
+ }else if(age <= 12 && genero === "masculino")
+    console.log("Eres un niño");
+  else if((age > 12 && age < 18) && genero === "femenino")
+    console.log("Eres una adolescente");
+  else if((age > 12 && age < 18) && genero === "masculino")
+    console.log("Eres un adolescente");
+  else if(age >= 18 && genero === "femenino")
+    console.log("Eres una mujer");
+  else if(age >=18 && genero === "masculino")
+    console.log("Eres un hombre");
+}
+
+main();
