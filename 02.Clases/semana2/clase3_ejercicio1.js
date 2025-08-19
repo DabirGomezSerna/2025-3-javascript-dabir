9636ce0a099db3a0272451d504b177b1671cb12b
DabirGomezSerna <dasego.gomezserna@gmail.com>
2025-08-18 20:28:48 -0600

    Tidying up 2

--- 02.Clases/semana2/clase3_ejercicio1.js
+++ 02.Clases/semana2/clase3_ejercicio1.js
@@ -1,28 +1,21 @@
-const { ask } = require('../helpers/input.js');
+const { ask } = require("../helpers/input.js");
 
-function saludar(nombre){
-    console.log(`Hola estimad@: ${nombre}, espero que tengas un buen día`);
+function saludar(nombre) {
+  console.log(`Hola estimad@: ${nombre}, espero que tengas un buen día`);
 }
 
 async function main() {
-    const personas = [
-        "Diego",
-        "Edgar",
-        "Irving",
-        "Jesus",
-        "Jorge",
-        "Luigy"
-    ];
+  const personas = ["Diego", "Edgar", "Irving", "Jesus", "Jorge", "Luigy"];
 
-    console.log("----------Ascending order----------");
-    for(let i = 0; i < personas.length; i++){
-        saludar(personas[i]);
-    }
+  console.log("----------Ascending order----------");
+  for (let i = 0; i < personas.length; i++) {
+    saludar(personas[i]);
+  }
 
-    console.log("----------Descending order----------");
-    for(let j = personas.length-1; j>=0; j--){
-        saludar(personas[j]);
-    }
+  console.log("----------Descending order----------");
+  for (let j = personas.length - 1; j >= 0; j--) {
+    saludar(personas[j]);
+  }
 }
 
 main();
