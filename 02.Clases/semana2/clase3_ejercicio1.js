d94425a6432f1bac49dc7909c22a94cc578adda8
DabirGomezSerna <dasego.gomezserna@gmail.com>
2025-08-18 20:03:50 -0600

    Array organized alphabetically

--- 02.Clases/semana2/clase3_ejercicio1.js
+++ 02.Clases/semana2/clase3_ejercicio1.js
@@ -6,12 +6,12 @@ function saludar(nombre){
 
 async function main() {
     const personas = [
-        "Edgar",
-        "Luigy",
         "Diego",
+        "Edgar",
         "Irving",
         "Jesus",
-        "Jorge"
+        "Jorge",
+        "Luigy"
     ];
 
     for(let i = 0; i < personas.length; i++){
